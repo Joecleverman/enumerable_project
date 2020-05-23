994f28fc656f20d0f5d58f525ce4c3cd2916980e
a2josue@yahoo.com <a2josue@yahoo.com>
2020-05-23 13:09:47 -0400

    Creating test for my_select

--- enumerable_test.rb
+++ enumerable_test.rb
@@ -6,7 +6,26 @@ end
 
 # my_each
 puts 'Test for #my_each'
-%w[Sally Marchony Bill].my_each { |x| print x, ' -- ' }
+%w[Sally Marchony Bill].my_each { |e| print e, ' -- ' }
 puts
+
+
+# my_each_with_index
+puts 'Test for #my_each_with_index'
+hash = {}
+%w[Jean dog Etienne].my_each_with_index { |element, index| hash[element] = index }
+print hash
+puts
+(1..5).my_each_with_index { |element, index| puts "#{element} => #{index}" }
+puts
+[1, 2, 3, 4, 5].my_each_with_index { |element, index| puts "#{element} => #{index}" }
+puts
+
+
+# my_select
+puts 'Test for #my_select'
+print([2, 1, 6, 7, 4, 8, 10].my_select(&:even?))
+puts
+
 
 
