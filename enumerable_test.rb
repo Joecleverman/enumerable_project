df0230e66447bb0f48964c7b00de592ebb467a55
a2josue@yahoo.com <a2josue@yahoo.com>
2020-05-23 13:42:55 -0400

    Creating test for my_all

--- enumerable_test.rb
+++ enumerable_test.rb
@@ -6,19 +6,19 @@ end
 
 # my_each
 puts 'Test for #my_each'
-%w[Sally Marchony Bill].my_each { |e| print e, ' -- ' }
+%w[Sally Marchony Bill].my_each { |x| print x, ' -- ' }
 puts
 
 
 # my_each_with_index
 puts 'Test for #my_each_with_index'
 hash = {}
-%w[Jean dog Etienne].my_each_with_index { |element, index| hash[element] = index }
+%w[Jean dog Etienne].my_each_with_index { |item, index| hash[item] = index }
 print hash
 puts
-(1..5).my_each_with_index { |element, index| puts "#{element} => #{index}" }
+(1..5).my_each_with_index { |item, index| puts "#{item} => #{index}" }
 puts
-[1, 2, 3, 4, 5].my_each_with_index { |element, index| puts "#{element} => #{index}" }
+[1, 2, 3, 4, 5].my_each_with_index { |item, index| puts "#{item} => #{index}" }
 puts
 
 
@@ -27,5 +27,19 @@ puts 'Test for #my_select'
 print([2, 1, 6, 7, 4, 8, 10].my_select(&:even?))
 puts
 
+
+# my_all
+puts 'Test for #my_all'
+puts(%w[Marc Luc Jean].my_all? { |word| word.length >= 3 })
+puts(%w[Marc Luc Jean].my_all? { |word| word.length >= 4 })
+puts [2, 1, 6, 7, 4, 8, 10].my_all?(3)
+puts %w[Marc Luc Jean].my_all?('Jean')
+puts %w[Marc Luc Jean].my_all?(/a/)
+puts [1, 5i, 5.67].my_all?(Numeric)
+puts [2, 1, 6, 7, 4, 8, 10].my_all?(Integer)
+puts [nil, true, 99].my_all?
+puts [nil, false].my_all?
+puts [nil, nil, nil].my_all?
+puts [].my_all?
 
 
